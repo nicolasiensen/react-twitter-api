53fa8ea2dc71cb0334eab91938c83ba3d343e52d
Nícolas Iensen <nicolas.iensen@gmail.com>
2017-04-08 20:56:54 +0200

    Add association: user has many access_tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  has_many :access_tokens
   validates :uuid, presence: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should validate attributes" do
+  test "should validate presence of uuid" do
     user = User.create
-    assert user.errors[:uuid].include?("can't be blank"), "should validate presence of uuid"
+    assert user.errors[:uuid].include?("can't be blank")
+  end
 
+  test "should validate uniqueness of uuid" do
     User.create(uuid: "123")
     user = User.create(uuid: "123")
-    assert user.errors[:uuid].include?("has already been taken"), "should validate uniqueness of uuid"
+    assert user.errors[:uuid].include?("has already been taken")
+  end
+
+  test "should have many access tokens" do
+    user = User.create(uuid: "123")
+    AccessToken.create(user_id: user.id, token: "123")
+    AccessToken.create(user_id: user.id, token: "321")
+    assert_equal user.access_tokens.count, 2
   end
 end
